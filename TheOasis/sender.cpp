944054130bdf4c00e5094000ffa1a31b76c001fc
brent <brent.gerets@gmail.com>
2023-05-30 18:28:53 +0200

    removed general info message

--- TheOasis/sender.cpp
+++ TheOasis/sender.cpp
@@ -34,29 +34,20 @@ void Sender::sendMessage(QString message)
  */
 void Sender::sendInfo(QList<QString> request)
 {
-    if (request.size() < 3 || request[2].compare("") == 0) {
-        QString response = QString("theoasis>info!>\n\
->>> The Oasis: Info <<<\n\n\
-Welcome to The Oasis, where your wildest fantasies become reality. Step into our virtual casino and leave behind the drudgery of \
-the real world. Our advanced technology allows you to experience the thrill of high-stakes gambling without ever leaving your home.\
-Don't let the mundane nature of your everyday life hold you back. Join us now and indulge in every desire you've ever had. \
-The Oasis – where reality is just a distant memory.\n\n\
-To join The Oasis, send the following request 'theoasis>register?>[username:string]>[password:string]>' and subscribe to 'theoasis>register!>[username:string]>'. \
-Make sure to hash your password for your own security.\n\n\
-To get an overview of all the possible request, send the following request 'theoasis>help?>' and subscribe to 'theoasis>help!>'.\n\
-");
-        sendMessage(response);
+    if (request.size() > 3) {
+        if (request[2].compare("slotmachine") == 0)
+            sendMessage(SlotMachine::getInfo());
+        else if (request[2].compare("roulette") == 0)
+            sendMessage(Roulette::getInfo());
+        else if (request[2].compare("blackjack") == 0)
+            sendMessage(Blackjack::getInfo());
+        else if (request[2].compare("cho-han") == 0)
+            sendMessage(ChoHan::getInfo());
+        else
+            throw FailedRequest(QString("theoasis>info!>" + request[2] + ">false>No info on this subject.>"));
     }
-    else if (request[2].compare("slotmachine") == 0)
-        sendMessage(SlotMachine::getInfo());
-    else if (request[2].compare("roulette") == 0)
-        sendMessage(Roulette::getInfo());
-    else if (request[2].compare("blackjack") == 0)
-        sendMessage(Blackjack::getInfo());
-    else if (request[2].compare("cho-han") == 0)
-        sendMessage(ChoHan::getInfo());
     else
-        throw FailedRequest(QString("theoasis>info!>" + request[2] + ">false>No info on this subject.>"));
+        throw FailedRequest(QString("theoasis>info!>" + request[2] + ">false>Specify a subject to get more info on.>"));
 }
 
 void Sender::sendLog(QString message, bool received)
